1b3d1be8f5f942401e2b6c1ab7774d778f822d66
Austin Johnson <technicallyaustin@gmail.com>
2020-01-27 19:27:59 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,7 @@
 require_relative "./vehicle.rb"
 class Car < Vehicle
   attr_accessor :Wheel_size, :number,
+  
   def initialize(wheel_size, number)
     @wheel_size = wheel_size
     @number = number 
