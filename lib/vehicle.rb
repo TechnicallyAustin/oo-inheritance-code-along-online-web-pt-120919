c51c5865972bd60287155ba4633ac72911567290
Austin Johnson <technicallyaustin@gmail.com>
2020-01-27 19:32:28 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,5 +1,13 @@
 class Vehicle
- 
+  attr_accessor :Wheel_size, :number, :wheel_number
+  def initialize(wheel_size, number)
+    @wheel_size = wheel_size
+    @number = number 
+  end
   
-
-end
+  def go 
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+  
+  def filling_up_tank
+    "filling up!"
